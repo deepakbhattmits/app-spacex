3870e5d1315e50be563d7dbfb0511ac1f06dd698
deepakbhattmits <deepakbhattmits@gmail.com>
2022-07-22 14:31:27 +0530

    Update CustomTable.tsx

--- components/CustomTable.tsx
+++ components/CustomTable.tsx
@@ -1,5 +1,6 @@
-import { Table, Tooltip } from "antd";
+import { Modal, Table, List, Tooltip } from "antd";
 import { FC, useEffect, useState } from "react";
+import QRCode from "react-qr-code";
 import { toast } from "react-toastify";
 import { columns, pagination } from "./constants";
 interface DataType {
@@ -20,8 +21,14 @@ interface IProp {
 
 [];
 const CustomTable: FC<IProp> = ({ datas }) => {
+  const [selectedFields, setSelectedFields] = useState<any[]>([]);
   const [filteredData, setFilteredData] = useState<DataType[]>([]);
+
+  const [selectedRow, setSelectedRow] = useState<any[]>([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState<number[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const [isQRModalVisible, setIsQRModalVisible] = useState<boolean>(false);
 
   const handleRowSelection = (recordId: number) => {
     if (selectedRowKeys?.includes(recordId)) {
@@ -61,6 +68,21 @@ const CustomTable: FC<IProp> = ({ datas }) => {
       .replace("PM", "");
     return date;
   };
+
+  const handelRow = (record?: any) => {
+    const { description, launch_date_utc, mission_id, mission_name } = record;
+    let updatedRecord: any = {
+      description,
+      launch_date_utc,
+      mission_id,
+      mission_name,
+    };
+    setSelectedRow(updatedRecord);
+  };
+  const handleQRModal = (record: any) => {
+    // console.log("this is clicked row record :", record);
+    setIsModalVisible(true);
+  };
   const renderColumns = () => {
     const renderColumns: any = columns?.map((column: any) => {
       return {
@@ -95,6 +117,17 @@ const CustomTable: FC<IProp> = ({ datas }) => {
                 </span>
               </label>
             )
+          : !!column?.dataIndex?.match(/qr/i)
+          ? (text: string, record: any) => {
+              return (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => handleQRModal(record)}
+                >
+                  {column?.title}
+                </button>
+              );
+            }
           : (text: string) => (
               <Tooltip placement="top" title={text}>
                 {text}
@@ -127,26 +160,111 @@ const CustomTable: FC<IProp> = ({ datas }) => {
       setFilteredData(updatedDatas);
     }
   }, [filteredData, datas]);
+  // useEffect(() => {
+  //   if (isModalVisible) {
+  //     handelRow();
+  //   }
+  // }, [isModalVisible]);
   return (
-    <Table
-      dataSource={filteredData}
-      locale={{ emptyText: "Not Found" }}
-      //TODO : make sure in the selected row add a class
-      // rowClassName={`${filteredData?.map((el) =>
-      //   selectedRowKeys?.includes(el?.id) ? "selected" : null
-      // )}`}
-      columns={renderColumns()}
-      onRow={(record, rowIndex) => {
-        return {
-          // onClick: (event) => handleRowSelection(record), // click row
-          // onDoubleClick: event => {}, // double click row
-          // onContextMenu: event => {}, // right button click row
-          // onMouseEnter: event => {}, // mouse enter row
-          // onMouseLeave: event => {}, // mouse leave row
-        };
-      }}
-      pagination={pagination}
-    />
+    <>
+      <Table
+        dataSource={filteredData}
+        locale={{ emptyText: "Not Found" }}
+        //TODO : make sure in the selected row add a class
+        // rowClassName={`${filteredData?.map((el) =>
+        //   selectedRowKeys?.includes(el?.id) ? "selected" : null
+        // )}`}
+        columns={renderColumns()}
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => handelRow(record), // click row
+            // onDoubleClick: event => {}, // double click row
+            // onContextMenu: event => {}, // right button click row
+            // onMouseEnter: event => {}, // mouse enter row
+            // onMouseLeave: event => {}, // mouse leave row
+          };
+        }}
+        pagination={pagination}
+      />
+      <Modal
+        title="QR code generator"
+        centered
+        visible={isModalVisible}
+        onOk={() => setIsModalVisible(false)}
+        onCancel={() => setIsModalVisible(false)}
+      >
+        <List
+          header={
+            <div className="flex gap-4 justify-between">
+              <span>Select Fields</span>
+
+              <span className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+                <svg
+                  className="w-3 h-3 mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                Selected fileds-
+                {selectedFields?.length}
+              </span>
+            </div>
+          }
+          footer={
+            !!selectedFields?.length ? (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => {
+                  setIsQRModalVisible(true);
+                }}
+              >
+                Generate QR code
+              </button>
+            ) : null
+          }
+          bordered
+          dataSource={Object?.entries(selectedRow)}
+          renderItem={(item) => (
+            <List.Item className="flex gap-2">
+              <label className="inline-flex items-center">
+                <input
+                  className="w-4 h-4 rounded border border-blue-300 focus:ring-3 "
+                  required
+                  type="checkbox"
+                  onChange={(event) => {
+                    let updatedList = [...selectedFields];
+                    if (event.target.checked) {
+                      updatedList = [...selectedFields, item["1"]];
+                    } else {
+                      updatedList.splice(selectedFields.indexOf(item["1"]), 1);
+                    }
+                    setSelectedFields(updatedList);
+                  }}
+                  // checked={selectedRowKeys?.includes(text)}
+                />
+                <span className="ml-2">{item["0"]}</span>
+              </label>
+              <span>{item["1"]}</span>
+            </List.Item>
+          )}
+        />
+      </Modal>
+      <Modal
+        title={`QR code for ${selectedFields?.map((el) => el)?.join(",")}`}
+        centered
+        visible={isQRModalVisible}
+        onOk={() => setIsQRModalVisible(false)}
+        onCancel={() => setIsQRModalVisible(false)}
+      >
+        <QRCode className="m-auto" value={selectedFields?.join("\n")} />
+      </Modal>
+    </>
   );
 };
 export default CustomTable;
